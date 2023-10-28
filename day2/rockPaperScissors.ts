8cbe41e9f7ea8b317e1ae95ff4a3746a30ac0245
Adam Virostek <adam.virostek@gmail.com>
2023-10-29 00:01:36 +0200

    add day2 solution

---
+++ day2/rockPaperScissors.ts
@@ -0,0 +1,18 @@
+import { readFromFile } from "../index";
+
+const scoringRules: { [key: string]: { [key: string]: number } } = {
+  A: { X: 3 + 1, Y: 6 + 2, Z: 0 + 3 },
+  B: { X: 0 + 1, Y: 3 + 2, Z: 6 + 3 },
+  C: { X: 6 + 1, Y: 0 + 2, Z: 3 + 3 },
+};
+
+readFromFile("input")
+  .then((data) => {
+    const scores = data.split("\n").map((value) => {
+      const [opponent, player] = value.split(" ");
+      return scoringRules[opponent]?.[player] || 0;
+    });
+    const totalScore = scores.reduce((prev, curr) => (prev ?? 0) + (curr ?? 0), 0);
+    console.log("Result score: " + totalScore);
+  })
+  .catch((err) => console.error(err));
